9b2eaa5f56c8a197c39fadebecfc8f64d8f6fd00
JorgeGL <jorgelogomez@gmail.com>
2022-12-15 19:42:18 +0100

    parte 13

--- resources/views/etiquetas/index.blade.php
+++ resources/views/etiquetas/index.blade.php
@@ -6,7 +6,8 @@
     <h1>Lista de etiquetas</h1>
 
     @foreach ($etiquetas as $etiqueta)
-            <li>{{$etiqueta}}</li>
-        @endforeach
+            <!-- <li>{{$etiqueta}}</li> -->
+            <li>nombre producto: {{$etiqueta->producto->name}}, marca: {{$etiqueta->producto->marca->name}}, registro sanitario: {{$etiqueta->producto->marca->registrosan}}</li>
+    @endforeach
 
 @endsection
